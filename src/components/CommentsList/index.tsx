81ed7812a9db6bda5dc508ee36435d6fe424d3f7
Filipe <filipefreire@protonmail.com>
2022-06-07 19:56:38 +0200

    add: minor CSS improvements

--- src/components/CommentsList/index.tsx
+++ src/components/CommentsList/index.tsx
@@ -20,12 +20,18 @@ export function CommentsList() {
       </div>
     );
   }
-  if (error) <p>Oops, looks like we have an error fetching data!</p>;
+
+  if (error)
+    return (
+      <p style={{ textAlign: "center" }}>
+        Oops, there was an error fetching data! 👀
+      </p>
+    );
 
   return (
-    <>
+    <section style={{ marginBottom: "1rem" }}>
       {comments &&
         comments.map((el) => <CommentCard key={el._id} comment={el.comment} />)}
-    </>
+    </section>
   );
 }
