85bba94ffefd9942e5b2dc778a874d8923bf0058
Filipe <filipefreire@protonmail.com>
2024-09-02 16:04:57 +0200

    chore: update deps

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
 
+// biome-ignore lint/style/noNonNullAssertion: root is defined on index.html
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
